9241989569d631e3509e5a966a2eaa742e99f098
Joeseph58 <Joeseph58@users.noreply.github.com>
2019-10-09 14:08:53 -0400

    datetime changes

--- ElevenNote.Service/NoteService.cs
+++ ElevenNote.Service/NoteService.cs
@@ -102,6 +102,21 @@ namespace ElevenNote.Service
         }
 
 
+        public bool DeleteNote(int noteId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Notes
+                        .Single(e => e.NoteId == noteId && e.OwnerId == _userId);
+
+                ctx.Notes.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
 
 
 
