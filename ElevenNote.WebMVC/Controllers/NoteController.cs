423216e599b56c15043b7d99a3faf5cde74a45e7
Joeseph58 <Joeseph58@users.noreply.github.com>
2019-10-09 11:03:08 -0400

    5.05 end push

---
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,42 @@
+﻿using EverNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View();
+        }
+
+        //Add method here VVVV
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Add code here vvvv
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
+
+
+
+    }
+}
